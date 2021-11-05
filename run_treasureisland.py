77adeeb175d12da88a90e655fa20a19c463c123d
Iddo <idoerg@gmail.com>
2021-11-05 14:31:03 -0500

    Changed gitignore and added argparge to run_treasureisland.pu

--- run_treasureisland.py
+++ run_treasureisland.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import sys
+import argparse
 from treasureisland.dna_sequence import sequence
 
-
+parser = argparse.ArgumentParser()
+parser.add_argument('infile',type=argparse.FileType('r'))
 def main(seqfile):
     seq = sequence(seqfile)
     pred = seq.predict()
@@ -10,5 +12,6 @@ def main(seqfile):
 
 
 if __name__ == '__main__':
+    parser.parse_args()
     main(sys.argv[1])
 
